4566b63520af990b09a76e65274ddfaf9474857a
doraly <doralynavarro@gmail.com>
2023-07-19 13:51:34 +0200

    Added first view with list of Posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
+from django.utils import timezone
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {"posts": posts})
